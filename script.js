37e1c6f83dfd5008176255f711b198489fb67483
vivek chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-04-29 21:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //your code here
 let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
-arr.sort()
-consol.log(arr)
+//arr.sort()
+//consol.log(arr)
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
+}
+
+const sortedTouristSpots  = arr.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+
+document.querySelector('#bands').innerHTML = 
+    sortedTouristSpots.map(item => `<li>${item}</li>`).join('')
